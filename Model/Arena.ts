810b8440b010419254620e030121eadfcced7b64
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-02-22 18:43:28 -0300

    Ajuste nos metodos e atributos da "Arena"

--- Model/Arena.ts
+++ Model/Arena.ts
@@ -8,5 +8,15 @@ module Model {
         public context: any;
         public width: number;
         public height: number;
+        public cellWidth: number;
+        
+        //Lets first create a generic function to paint cells
+        public paint_cell(x:number, y:number)
+        {
+            this.context.fillStyle = "blue";
+            this.context.fillRect(x*this.cellWidth, y*this.cellWidth, this.cellWidth, this.cellWidth);
+            this.context.strokeStyle = "white";
+            this.context.strokeRect(x*this.cellWidth, y*this.cellWidth, this.cellWidth, this.cellWidth);
+        }
     }
 }
